a465e746309a20eb5cdfc7b8687c3f83a3474470
amenasse <amenasse@gmail.com>
2016-03-09 17:37:50 +1100

    Hello moodi

---
+++ moodi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for moodi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moodi.settings")
+
+application = get_wsgi_application()
